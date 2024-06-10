d0723e7599fb58bc99e00577d5d57edf73f00352
alabrous <axel.labrousse@protonmail.com>
2024-06-11 01:19:42 +0200

    landmarks arrays capture

---
+++ main.py
@@ -0,0 +1,54 @@
+import cv2
+import mediapipe as mp
+import numpy as np
+
+mp_holistic = mp.solutions.holistic
+mp_drawing = mp.solutions.drawing_utils
+
+def mediapipe_detection(image, model):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image.flags.writeable = False
+    results = model.process(image)
+    image.flags.writeable = True
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    return image, results
+
+def draw_styled_landmarks(image, results):
+    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
+                              mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=4),
+                                mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2))
+    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                                                            mp_drawing.DrawingSpec(color=(0,0,0), thickness=2, circle_radius=4),
+                                mp_drawing.DrawingSpec(color=(0,0,0), thickness=2, circle_radius=2))
+    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                                                            mp_drawing.DrawingSpec(color=(255,255,255), thickness=2, circle_radius=4),
+                                mp_drawing.DrawingSpec(color=(255,255,255), thickness=2, circle_radius=2))
+
+def capture_webcam():
+    cap = cv2.VideoCapture(0)
+    with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if not ret:
+                break
+            
+            image , results = mediapipe_detection(frame, holistic)
+            draw_styled_landmarks(image, results)
+
+            pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(33*4)
+            lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21*3)
+            rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(21*3)
+            print(pose)
+            print(lh)
+            print(rh)
+
+            cv2.imshow('frame', image)
+            if cv2.waitKey(10) & 0xFF == ord('q'):
+                break
+    cap.release()
+    cv2.destroyAllWindows()
+
+    
+
+if __name__ == '__main__':
+    capture_webcam()
