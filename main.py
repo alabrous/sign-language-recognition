b866a60ddaaaab88f315f73fe3dafa14332d1fef
alabrous <axel.labrousse@protonmail.com>
2024-06-12 22:13:20 +0200

    adding face landmarks

--- main.py
+++ main.py
@@ -18,11 +18,14 @@ def draw_styled_landmarks(image, results):
                               mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=4),
                                 mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2))
     mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
-                                                            mp_drawing.DrawingSpec(color=(0,0,0), thickness=2, circle_radius=4),
-                                mp_drawing.DrawingSpec(color=(0,0,0), thickness=2, circle_radius=2))
+                                                            mp_drawing.DrawingSpec(color=(255,255,255), thickness=2, circle_radius=4),
+                                mp_drawing.DrawingSpec(color=(255,0,0), thickness=2, circle_radius=2))
     mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
                                                             mp_drawing.DrawingSpec(color=(255,255,255), thickness=2, circle_radius=4),
-                                mp_drawing.DrawingSpec(color=(255,255,255), thickness=2, circle_radius=2))
+                                mp_drawing.DrawingSpec(color=(0,0,255), thickness=2, circle_radius=2))
+    mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_CONTOURS,
+                                                            mp_drawing.DrawingSpec(color=(80,110,10), thickness=1, circle_radius=1),
+                                mp_drawing.DrawingSpec(color=(80,256,121), thickness=1, circle_radius=1))
 
 def capture_webcam():
     cap = cv2.VideoCapture(0)
@@ -38,9 +41,11 @@ def capture_webcam():
             pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(33*4)
             lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21*3)
             rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(21*3)
+            face = np.array([[res.x, res.y, res.z] for res in results.face_landmarks.landmark]).flatten() if results.face_landmarks else np.zeros(468*3)
             print(pose)
             print(lh)
             print(rh)
+            print(face)
 
             cv2.imshow('frame', image)
             if cv2.waitKey(10) & 0xFF == ord('q'):
